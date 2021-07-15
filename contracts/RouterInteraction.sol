1a261985f9e4db92da4ebad1bca136ab59f680a7
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2021-07-15 14:24:34 +0500

    build: swap interaction with v2 uniswap router

--- contracts/RouterInteraction.sol
+++ contracts/RouterInteraction.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.7.6;
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 contract RouterInteraction {
     using SafeERC20 for IERC20;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -76,7 +76,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.6",
+    version: "0.7.6",
     settings: {
       metadata: {
         // Not including the metadata hash
